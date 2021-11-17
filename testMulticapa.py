d7401a6fedbdf21042ba36a9bd485a013bf94645
Marcos José Cecotti <marcosjcecotti@hotmail.com>
2021-11-17 11:25:43 -0300

    Update testMulticapa.py

--- testMulticapa.py
+++ testMulticapa.py
@@ -1,25 +1,15 @@
 import math
 import numpy as np
-from matplotlib import pyplot as plt
 
-
-def test(df,W,capas):
-    X = df.iloc[:, 0:-1]
-    yd = df.iloc[:, -1]
-
-    X = X.to_numpy()
-    yd = yd.to_numpy()
+def test(X,yd,W,capas):
+    cantCapas = len(capas)
 
     cantPruebas = len(X)
     cantEntradas = len(X[0])
-    cantCapas = len(capas)
+    b = 1
     e = math.e
-
-    columnBIAS = (np.full((cantPruebas, 1), -1))
-    X = np.hstack((columnBIAS, X))  # reagrupamos
-    b=1
-    error = 0
-    vectorConfusion = []
+    # Comparo las salidas y calculo la cantidad de aciertos en una epoca
+    acierto = 0
     for i in range(cantPruebas):
         yCapaBIAS = []
         yCapaBIAS.insert(0, X[i][:].tolist())  # Insertamos las entradas
@@ -32,37 +22,21 @@ def test(df,W,capas):
                 yAux.insert(k, sigmoide)
             yAux.insert(0, -1)
             yCapaBIAS.insert(j + 1, yAux)
-        y = yCapaBIAS[cantCapas][1]  # Salida final
-        # Funcion signo
-        if y > 0:
-            y = 1
-        else:
-            y = -1
 
-        if y != yd[i]:
-            error = error + 1
+        y = yCapaBIAS[cantCapas][:]  # Salida final
+        # Regla de y
+        y = np.array(y[1:])
+        index = np.unravel_index(y.argmax(), y.shape)
+        for l in range(len(y)):
+            y[l] = -1
+        y[index] = 1
 
-        # Calculo de la matriz de confusion
-        if y == 1 and yd[i] == 1:
-            vectorConfusion.insert(i, 0)
-        if y == 1 and yd[i] == -1:
-            vectorConfusion.insert(i, 1)
-        if y == -1 and yd[i] == -1:
-            vectorConfusion.insert(i, 2)
-        if y == -1 and yd[i] == 1:
-            vectorConfusion.insert(i, 3)
+        cont = 0
+        for m in range(len(y)):
+            if y[m] != yd[i][m]:
+                cont += 1
 
-    error = error / cantPruebas
-
-    plt.title('Datos en concentile TEST.csv')
-    plt.xlabel("X1")
-    plt.ylabel("X2")
-    plt.xlim(0, 1)
-    plt.ylim(0, 1)
-    plt.axhline(0, color="black")
-    plt.axvline(0, color="black")
-    vColor = ["green", "red", "blue", "black"]
-    for i in range(cantPruebas):
-        plt.plot(X[i][1], X[i][2], marker='+', color=vColor[vectorConfusion[i]])
-    plt.show()
-    return error
+        if cont == 0:
+            acierto += 1
+    acierto = (acierto / cantPruebas) * 100
+    return acierto
